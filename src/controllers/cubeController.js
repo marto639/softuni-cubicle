6398409cc8f103a202dfc6518af328b2516fac13
marto639 <martin639@abv.bg>
2022-06-15 07:36:54 +0300

    Add new rubic
    Add persistance

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
+const fs = require('fs/promises');
+
+const cubes = require('../db.json');
 
 router.get('/create', (req, res) => {
     res.render('create');
+});
+
+router.post('/create', (req, res) => {
+    const cube = req.body;
+
+    // Validate
+    if (cube.name.length < 2) {
+        return res.status(400).send('Invalid request');
+    }
+    // Save data
+    cubes.push(cube);
+    fs.writeFile('src/db.json', JSON.stringify(cubes, '', 4), { encoding: 'utf-8' })
+        .then(() => {
+            // Redirect page
+            res.redirect('/');
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        })
 });
 
 module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,7 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 
-router.get('/', homeController.index);
-router.get('/about', homeController.about);
-
+router.use('/', homeController);
 router.use('/cube', cubeController);
 
 module.exports = router;
