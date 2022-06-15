67c46b512cb3c853d157c58f91e33e5caa861d34
marto639 <martin639@abv.bg>
2022-06-15 08:41:39 +0300

    Render static details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,29 +1,29 @@
 const router = require('express').Router();
-const fs = require('fs/promises');
-
-const cubes = require('../db.json');
+const cubeService = require('../services/cubeService');
 
 router.get('/create', (req, res) => {
     res.render('create');
 });
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     const cube = req.body;
 
     // Validate
     if (cube.name.length < 2) {
         return res.status(400).send('Invalid request');
     }
-    // Save data
-    cubes.push(cube);
-    fs.writeFile('src/db.json', JSON.stringify(cubes, '', 4), { encoding: 'utf-8' })
-        .then(() => {
-            // Redirect page
-            res.redirect('/');
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        })
+
+    try {
+        await cubeService.save(cube);
+
+        res.redirect('/');
+    } catch (error) {
+        res.status(400).send(err);
+    }
+});
+
+router.get('/details/:id', (req, res) => {
+    res.render('details');
 });
 
 module.exports = router;
